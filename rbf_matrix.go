d4676feb1f13bc01575cb7fe7038d7dac7b6f814
Alex Nichol <unixpickle@gmail.com>
2016-12-26 17:50:49 -0500

    improve speed using RBF cache
    The benchmark shows a ~3x speedup.

--- rbf_matrix.go
+++ rbf_matrix.go
@@ -8,6 +8,11 @@ import (
 
 const epsilon = 1e-8
 
+type rbfCacheEntry struct {
+	InIndex int
+	Coeff   float64
+}
+
 // A rbfMatrix acts on behalf of an RBF layer with centers
 // arranged on the points in an image.
 // The Variance is used to divide the argument to exp,
@@ -17,7 +22,7 @@ type rbfMatrix struct {
 	Width    int
 	Height   int
 
-	rbfCache [][]float64
+	cache [][]rbfCacheEntry
 }
 
 // Dim returns the total number of pixels.
@@ -29,49 +34,45 @@ func (n *rbfMatrix) Dim() int {
 // definite matrix Gij = Rho(Pi-Pj), where Pi is the i-th
 // point in the image.
 func (n *rbfMatrix) Apply(in linalg.Vector) linalg.Vector {
-	result := make(linalg.Vector, 0, n.Dim())
-	for y := 0; y < n.Height; y++ {
-		for x := 0; x < n.Width; x++ {
-			applied := n.applyPoint(x, y, in)
-			result = append(result, applied)
+	if n.cache == nil {
+		n.buildCache()
+	}
+	result := make(linalg.Vector, n.Dim())
+	for i, caches := range n.cache {
+		for _, c := range caches {
+			result[i] += in[c.InIndex] * c.Coeff
 		}
 	}
 	return result
 }
 
-func (n *rbfMatrix) applyPoint(x, y int, in linalg.Vector) float64 {
+func (n *rbfMatrix) buildCache() {
+	n.cache = make([][]rbfCacheEntry, 0, n.Width*n.Height)
+	for y := 0; y < n.Height; y++ {
+		for x := 0; x < n.Width; x++ {
+			n.cache = append(n.cache, n.buildPixelCache(x, y))
+		}
+	}
+}
+
+func (n *rbfMatrix) buildPixelCache(x, y int) []rbfCacheEntry {
 	minX, minY, maxX, maxY := pointBounds(float64(x), float64(y), n.Variance)
-	var sum float64
+	var res []rbfCacheEntry
 	for i := minY; i < maxY && i < n.Height; i++ {
 		for j := minX; j < maxX && j < n.Width; j++ {
 			scale := n.rbf(x, y, j, i)
-			sum += scale * in[j+i*n.Width]
+			res = append(res, rbfCacheEntry{
+				InIndex: j + i*n.Width,
+				Coeff:   scale,
+			})
 		}
 	}
-	return sum
+	return res
 }
 
 func (n *rbfMatrix) rbf(x1, y1, x2, y2 int) float64 {
-	if n.rbfCache == nil {
-		maxDist := 2 * int(math.Sqrt(-math.Log(epsilon)*2*n.Variance)+1)
-		n.rbfCache = make([][]float64, maxDist)
-		for i := range n.rbfCache {
-			n.rbfCache[i] = make([]float64, maxDist)
-			for j := range n.rbfCache[i] {
-				dist := float64(i*i + j*j)
-				rbf := math.Exp(-dist / (2 * n.Variance))
-				n.rbfCache[i][j] = rbf
-			}
-		}
-	}
-	return n.rbfCache[absInt(x1-x2)][absInt(y1-y2)]
-}
-
-func absInt(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
+	dist := float64((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2))
+	return math.Exp(-dist / (2 * n.Variance))
 }
 
 func pointBounds(x, y, variance float64) (minX, minY, maxX, maxY int) {
